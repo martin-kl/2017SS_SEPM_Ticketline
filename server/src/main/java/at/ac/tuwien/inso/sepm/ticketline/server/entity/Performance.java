e987bc5d923b95478f248ad57bb57a1e0224656e
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2017-04-22 22:18:59 +0200

    refactor(entity): remove unnecessary import

--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/entity/Performance.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/entity/Performance.java
@@ -8,7 +8,6 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.time.LocalDate;
 import java.util.Set;
 import java.util.UUID;
 
