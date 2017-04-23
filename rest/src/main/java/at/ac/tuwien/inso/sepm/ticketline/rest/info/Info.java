4489500af89d7cc729bf16d563fb962bc1260767
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2017-04-23 23:24:08 +0200

    refactor(rest): refactor models

--- rest/src/main/java/at/ac/tuwien/inso/sepm/ticketline/rest/info/Info.java
+++ rest/src/main/java/at/ac/tuwien/inso/sepm/ticketline/rest/info/Info.java
@@ -1,31 +1,22 @@
 package at.ac.tuwien.inso.sepm.ticketline.rest.info;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.Duration;
 import java.time.ZonedDateTime;
 
+@Getter
+@Setter
 public class Info {
 
     private Git git;
     private Duration uptime;
 
-    public Git getGit() {
-        return git;
-    }
-
-    public void setGit(Git git) {
-        this.git = git;
-    }
-
-    public Duration getUptime() {
-        return uptime;
-    }
-
-    public void setUptime(Duration uptime) {
-        this.uptime = uptime;
-    }
-
+    @Getter
+    @Setter
     public static class Git {
 
         private String branch;
@@ -33,38 +24,8 @@ public class Info {
         private Build build;
         private Commit commit;
 
-        public String getBranch() {
-            return branch;
-        }
-
-        public void setBranch(String branch) {
-            this.branch = branch;
-        }
-
-        public String getTags() {
-            return tags;
-        }
-
-        public void setTags(String tags) {
-            this.tags = tags;
-        }
-
-        public Build getBuild() {
-            return build;
-        }
-
-        public void setBuild(Build build) {
-            this.build = build;
-        }
-
-        public Commit getCommit() {
-            return commit;
-        }
-
-        public void setCommit(Commit commit) {
-            this.commit = commit;
-        }
-
+        @Getter
+        @Setter
         public static class Build {
 
             private String version;
@@ -72,23 +33,10 @@ public class Info {
             @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
             private ZonedDateTime time;
 
-            public String getVersion() {
-                return version;
-            }
-
-            public void setVersion(String version) {
-                this.version = version;
-            }
-
-            public ZonedDateTime getTime() {
-                return time;
-            }
-
-            public void setTime(ZonedDateTime time) {
-                this.time = time;
-            }
         }
 
+        @Getter
+        @Setter
         public static class Commit {
 
             @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
@@ -96,33 +44,12 @@ public class Info {
 
             private Id id;
 
-            public ZonedDateTime getTime() {
-                return time;
-            }
-
-            public void setTime(ZonedDateTime time) {
-                this.time = time;
-            }
-
-            public Id getId() {
-                return id;
-            }
-
-            public void setId(Id id) {
-                this.id = id;
-            }
-
+            @Getter
+            @Setter
             public static class Id {
 
                 private String abbrev;
 
-                public String getAbbrev() {
-                    return abbrev;
-                }
-
-                public void setAbbrev(String abbrev) {
-                    this.abbrev = abbrev;
-                }
             }
         }
     }
--- rest/src/main/java/at/ac/tuwien/inso/sepm/ticketline/rest/news/DetailedNewsDTO.java
+++ rest/src/main/java/at/ac/tuwien/inso/sepm/ticketline/rest/news/DetailedNewsDTO.java
@@ -2,16 +2,18 @@ package at.ac.tuwien.inso.sepm.ticketline.rest.news;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @Setter
+@ToString
+@Builder
 @EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 @ApiModel(value = "DetailedNewsDTO", description = "A detailed DTO for news entries via rest")
 public class DetailedNewsDTO {
 
@@ -27,54 +29,4 @@ public class DetailedNewsDTO {
     @ApiModelProperty(required = true, name = "The text content of the news")
     private String text;
 
-    @Override
-    public String toString() {
-        return "DetailedNewsDTO{" +
-            "id=" + id +
-            ", publishedAt=" + publishedAt +
-            ", title='" + title + '\'' +
-            ", text='" + text + '\'' +
-            '}';
-    }
-
-    public static NewsDTOBuilder builder() {
-        return new NewsDTOBuilder();
-    }
-
-    public static final class NewsDTOBuilder {
-
-        private UUID id;
-        private LocalDateTime publishedAt;
-        private String title;
-        private String text;
-
-        public NewsDTOBuilder id(UUID id) {
-            this.id = id;
-            return this;
-        }
-
-        public NewsDTOBuilder publishedAt(LocalDateTime publishedAt) {
-            this.publishedAt = publishedAt;
-            return this;
-        }
-
-        public NewsDTOBuilder title(String title) {
-            this.title = title;
-            return this;
-        }
-
-        public NewsDTOBuilder text(String text) {
-            this.text = text;
-            return this;
-        }
-
-        public DetailedNewsDTO build() {
-            DetailedNewsDTO newsDTO = new DetailedNewsDTO();
-            newsDTO.setId(id);
-            newsDTO.setPublishedAt(publishedAt);
-            newsDTO.setTitle(title);
-            newsDTO.setText(text);
-            return newsDTO;
-        }
-    }
 }
