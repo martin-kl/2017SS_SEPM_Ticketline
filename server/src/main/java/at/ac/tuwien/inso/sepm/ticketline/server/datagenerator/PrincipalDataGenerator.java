f9a76c6b3b4d437930b2b2a599240aaa2277c8d0
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2017-04-23 21:06:15 +0200

    feature(data): add generator for seen news

--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/datagenerator/PrincipalDataGenerator.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/datagenerator/PrincipalDataGenerator.java
@@ -1,6 +1,10 @@
 package at.ac.tuwien.inso.sepm.ticketline.server.datagenerator;
 
+import at.ac.tuwien.inso.sepm.ticketline.server.entity.News;
 import at.ac.tuwien.inso.sepm.ticketline.server.entity.Principal;
+import at.ac.tuwien.inso.sepm.ticketline.server.entity.PrincipalNews;
+import at.ac.tuwien.inso.sepm.ticketline.server.repository.NewsRepository;
+import at.ac.tuwien.inso.sepm.ticketline.server.repository.PrincipalNewsRepository;
 import at.ac.tuwien.inso.sepm.ticketline.server.repository.PrincipalRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Slf4j
 @Profile("generateData")
@@ -21,26 +26,60 @@ public class PrincipalDataGenerator {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private NewsRepository newsRepository;
+
+    @Autowired
+    private PrincipalNewsRepository principalNewsRepository;
+
+    // injected so we make sure that news are loaded
+    @Autowired
+    private NewsDataGenerator newsDataGenerator;
+
     @PostConstruct
     private void generatePriceCategories() {
         if (principalRepository.count() > 0) {
             log.info("principals already generated");
         } else {
 
+            List<News> newsList = newsRepository.findAll();
+
             Principal admin = Principal.builder()
                 .role(Principal.Role.ADMIN)
                 .username("admin")
                 .password(passwordEncoder.encode("password"))
+                .enabled(true)
                 .build();
 
             Principal user = Principal.builder()
                 .role(Principal.Role.SELLER)
                 .username("user")
                 .password(passwordEncoder.encode("password"))
+                .enabled(true)
                 .build();
 
             principalRepository.save(admin);
             principalRepository.save(user);
+
+            // create principal news connections
+
+            for (News news : newsList) {
+                PrincipalNews principalNews = PrincipalNews.builder()
+                    .news(news)
+                    .principal(admin)
+                    .seen(false)
+                    .build();
+                principalNewsRepository.save(principalNews);
+            }
+
+            for (News news : newsList) {
+                PrincipalNews principalNews = PrincipalNews.builder()
+                    .news(news)
+                    .principal(user)
+                    .seen(false)
+                    .build();
+                principalNewsRepository.save(principalNews);
+            }
 
         }
     }
