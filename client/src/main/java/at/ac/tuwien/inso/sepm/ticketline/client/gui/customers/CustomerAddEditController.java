d40a391dd98c09b0c5dda7503271c2d31de4a607
Martin Klampfer <e1526110@student.tuwien.ac.at>
2017-04-27 17:33:37 +0200

    customer saving and exit and everything working

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/customers/CustomerAddEditController.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/customers/CustomerAddEditController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.stage.Modality;
@@ -19,6 +20,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomerAddEditController {
 
+    @FXML
+    private Button btn_CustomerCancel;
+    @FXML
+    private Button btn_CustomerOK;
     @FXML
     private TextField tf_customerFirstName;
     @FXML
@@ -33,17 +38,18 @@ public class CustomerAddEditController {
     }
 
     public void handleCustomerCancel(ActionEvent actionEvent) {
-        /*
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.initModality(Modality.APPLICATION_MODAL);
-        alert.initOwner(mainStage);
-        alert.setTitle(BundleManager.getBundle().getString("dialog.customer.cancel"));
+
+        alert.setTitle(BundleManager.getBundle().getString("dialog.customer.title"));
         alert.setHeaderText(BundleManager.getBundle().getString("dialog.customer.header"));
         alert.setContentText(BundleManager.getBundle().getString("dialog.customer.content"));
         Optional<ButtonType> result = alert.showAndWait();
-        if (!result.isPresent() || !ButtonType.OK.equals(result.get())) {
+
+        if (!result.isPresent() || ButtonType.OK.equals(result.get())) {
+            Stage stage = (Stage) btn_CustomerCancel.getScene().getWindow();
+            stage.close();
         }
-        */
     }
 
     public void handleCustomerOK(ActionEvent actionEvent) throws DataAccessException {
@@ -54,6 +60,11 @@ public class CustomerAddEditController {
         customerDTO.setFirstName(firstName);
         customerDTO.setLastName(lastName);
         customerDTO = customerService.save(customerDTO);
-        log.info("controller: customer after save method has first name = {}, last name = {} and id = {}", customerDTO.getFirstName(), customerDTO.getLastName(), customerDTO.getId());
+        log.info(
+            "controller: customer after save method has first name = {}, last name = {} and id = {}",
+            customerDTO.getFirstName(), customerDTO.getLastName(), customerDTO.getId());
+
+        Stage stage = (Stage) btn_CustomerCancel.getScene().getWindow();
+        stage.close();
     }
 }
