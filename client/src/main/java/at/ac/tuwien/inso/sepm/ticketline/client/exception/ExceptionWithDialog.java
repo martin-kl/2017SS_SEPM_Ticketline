4e5c59b86ac37747963063710285f8ab11930078
Calvin Claus <calvinclaus@me.com>
2017-05-01 14:09:37 +0200

    Alert on correct thread

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/exception/ExceptionWithDialog.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/exception/ExceptionWithDialog.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.inso.sepm.ticketline.client.exception;
 
 import at.ac.tuwien.inso.sepm.ticketline.client.util.BundleManager;
+import javafx.application.Platform;
 import javafx.scene.control.Alert;
 
 
@@ -18,10 +19,12 @@ public abstract class ExceptionWithDialog extends Exception {
     }
 
     public void showDialog() {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle(BundleManager.getBundle().getString("default.error.title"));
-        alert.setHeaderText(BundleManager.getBundle().getString("default.error.header"));
-        alert.setContentText(BundleManager.getBundle().getString(errorMessagePropertyName));
-        alert.showAndWait();
+        Platform.runLater(() -> {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(BundleManager.getBundle().getString("default.error.title"));
+            alert.setHeaderText(BundleManager.getBundle().getString("default.error.header"));
+            alert.setContentText(BundleManager.getBundle().getString(errorMessagePropertyName));
+            alert.showAndWait();
+        });
     }
 }
