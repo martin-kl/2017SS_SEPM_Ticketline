a559081256ae05426dbe37c4d90ab582b830a13e
Alex <a.schneider9494@gmail.com>
2017-05-03 00:01:44 +0200

    Added LocationDTO, finished PerformanceDTO

--- rest/src/main/java/at/ac/tuwien/inso/sepm/ticketline/rest/event/EventDTO.java
+++ rest/src/main/java/at/ac/tuwien/inso/sepm/ticketline/rest/event/EventDTO.java
@@ -1,9 +1,12 @@
 package at.ac.tuwien.inso.sepm.ticketline.rest.event;
+import at.ac.tuwien.inso.sepm.ticketline.rest.enums.EventCategory;
+import at.ac.tuwien.inso.sepm.ticketline.rest.performance.PerformanceDTO;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -23,16 +26,16 @@ public class EventDTO {
     @ApiModelProperty(required = true, readOnly = true, name = "The name of the event")
     private String name;
 
-    //TODO: Category enum?
-    /*@ApiModelProperty(required = true, readOnly = true, name = "The category of the event")
-    private Category category;*/
+
+    @ApiModelProperty(required = true, readOnly = true, name = "The category of the event")
+    private EventCategory category;
 
     @ApiModelProperty(required = true, readOnly = true, name = "The description of the event")
     private String description;
 
     /*@ApiModelProperty(required = true, readOnly = true, name = "The artists playing in the event")
-    private List<EventArtistDTO> artists;
+    private List<EventArtistDTO> artists;*/
 
     @ApiModelProperty(required = true, readOnly = true, name = "The performances for the event")
-    private List<PerformanceDTO> performances;*/
+    private List<PerformanceDTO> performances;
 }
