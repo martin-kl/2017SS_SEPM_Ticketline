270f22638807cc7e51f3454efba6b7a4474206e5
Alex <a.schneider9494@gmail.com>
2017-05-03 14:24:35 +0200

    Created EventEndpoint

--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/entity/mapper/event/EventMapper.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/entity/mapper/event/EventMapper.java
@@ -4,6 +4,7 @@ import at.ac.tuwien.inso.sepm.ticketline.rest.event.EventDTO;
 import at.ac.tuwien.inso.sepm.ticketline.server.entity.Event;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
     componentModel = "spring",
     unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
+@Component
 public interface EventMapper {
     Event fromDTO(EventDTO eventDTO);
 
