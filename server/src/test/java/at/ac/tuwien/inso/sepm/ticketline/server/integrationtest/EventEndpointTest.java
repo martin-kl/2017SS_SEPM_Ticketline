c5b33c91e02967449caaba428a65627d89865aa3
Akkes1 <a.schneider9494@gmail.com>
2017-05-06 10:11:41 +0200

    begin test fix

--- server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/integrationtest/EventEndpointTest.java
+++ server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/integrationtest/EventEndpointTest.java
@@ -1,11 +1,25 @@
 package at.ac.tuwien.inso.sepm.ticketline.server.integrationtest;
 
+import at.ac.tuwien.inso.sepm.ticketline.rest.enums.EventCategory;
+import at.ac.tuwien.inso.sepm.ticketline.rest.event.EventDTO;
+import at.ac.tuwien.inso.sepm.ticketline.server.entity.Event;
+import at.ac.tuwien.inso.sepm.ticketline.server.entity.Performance;
 import at.ac.tuwien.inso.sepm.ticketline.server.integrationtest.base.BaseIntegrationTest;
+import at.ac.tuwien.inso.sepm.ticketline.server.repository.EventRepository;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.UUID;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 
 import static org.hamcrest.core.Is.is;
@@ -13,6 +27,12 @@ import static org.hamcrest.core.Is.is;
 
 public class EventEndpointTest extends BaseIntegrationTest {
     private static final String EVENT_ENDPOINT = "/event";
+    private static final UUID TEST_EVENT_ID = UUID.randomUUID();
+    private static final String TEST_EVENT_NAME = "Testname";
+    private static final String TEST_EVENT_DESCRIPTION = "Testdescription";
+
+    @MockBean
+    private EventRepository eventRepository;
 
     //TODO: implement complete test
     @Test
@@ -24,5 +44,33 @@ public class EventEndpointTest extends BaseIntegrationTest {
             .then().extract().response();
         Assert.assertThat(response.getStatusCode(), is(HttpStatus.UNAUTHORIZED.value()));
     }
+
+   /* @Test
+    public void findAllEvents() {
+        BDDMockito.
+            given(eventRepository.findAll()).
+            willReturn(Collections.singletonList(
+                Event.builder()
+                    .id(TEST_EVENT_ID)
+                    .name(TEST_EVENT_NAME)
+                    .category(EventCategory.NO_CATEGORY)
+                    .description(TEST_EVENT_DESCRIPTION)
+                    .build()));
+
+        Response response = RestAssured
+            .given()
+            .contentType(ContentType.JSON)
+            .header(HttpHeaders.AUTHORIZATION, validUserTokenWithPrefix)
+            .when().get(EVENT_ENDPOINT)
+            .then().extract().response();
+        Assert.assertThat(response.getStatusCode(), is(HttpStatus.OK.value()));
+        Assert.assertThat(Arrays.asList(response.as(EventDTO[].class)), is(Collections.singletonList(
+            EventDTO.builder()
+                .id(TEST_EVENT_ID)
+                .name(TEST_EVENT_NAME)
+                .category(EventCategory.NO_CATEGORY)
+                .description(TEST_EVENT_DESCRIPTION)
+                .build())));
+    }*/
 
 }
