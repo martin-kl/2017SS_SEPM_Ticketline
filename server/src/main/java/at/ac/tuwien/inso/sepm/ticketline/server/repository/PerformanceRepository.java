ddbf1ff26458cdf60b7412da4f28b6b29cb213ba
Benjamin Schaden <benni.schaden@gmail.com>
2017-05-06 18:15:59 +0200

    added performanceEndpoint

--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/repository/PerformanceRepository.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/repository/PerformanceRepository.java
@@ -4,8 +4,16 @@ import at.ac.tuwien.inso.sepm.ticketline.server.entity.Performance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PerformanceRepository extends JpaRepository<Performance, UUID> {
+    /**
+     * Find a single performance by id
+     *
+     * @param id the is of the performance
+     * @return Optional containing the performance
+     */
+    Optional<Performance> findOneById(UUID id);
 }
