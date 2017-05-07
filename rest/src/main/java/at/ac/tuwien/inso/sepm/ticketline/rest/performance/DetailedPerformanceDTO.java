5707f650cabd78ef17411febebaa4f10f9ec1764
Benjamin Schaden <benni.schaden@gmail.com>
2017-05-07 18:29:18 +0200

    fixed jackson abstract mapping and added 2 methods in performance service

---
+++ rest/src/main/java/at/ac/tuwien/inso/sepm/ticketline/rest/performance/DetailedPerformanceDTO.java
@@ -0,0 +1,7 @@
+package at.ac.tuwien.inso.sepm.ticketline.rest.performance;
+
+/**
+ * Created by benni on 07.05.2017.
+ */
+public class DetailedPerformanceDTO {
+}
---
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/service/util/TicketWrapper.java
@@ -0,0 +1,7 @@
+package at.ac.tuwien.inso.sepm.ticketline.server.service.util;
+
+/**
+ * Created by benni on 07.05.2017.
+ */
+public class TicketWrapper {
+}
