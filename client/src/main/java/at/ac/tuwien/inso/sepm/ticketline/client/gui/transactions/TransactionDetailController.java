2ab5d62e4ce4ff2b0b9bd48eaa8f82a06462fdff
Calvin Claus <calvinclaus@me.com>
2017-05-08 15:41:07 +0200

    WIP

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/transactions/TransactionDetailController.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/transactions/TransactionDetailController.java
@@ -1,43 +1,22 @@
 package at.ac.tuwien.inso.sepm.ticketline.client.gui.transactions;
 
 import at.ac.tuwien.inso.springfx.SpringFxmlLoader;
-import javafx.scene.layout.VBox;<<<<<<< Updated upstream
-=======
-import at.ac.tuwien.inso.sepm.ticketline.client.exception.DataAccessException;
-import at.ac.tuwien.inso.sepm.ticketline.client.exception.ExceptionWithDialog;
-import at.ac.tuwien.inso.sepm.ticketline.client.gui.MainController;
-import at.ac.tuwien.inso.sepm.ticketline.client.gui.customers.CustomersElementController;
-import at.ac.tuwien.inso.sepm.ticketline.client.service.CustomerService;
+import javafx.scene.layout.VBox;
 import at.ac.tuwien.inso.sepm.ticketline.client.util.BundleManager;
-import at.ac.tuwien.inso.sepm.ticketline.client.util.JavaFXUtils;
-import at.ac.tuwien.inso.sepm.ticketline.rest.customer.CustomerDTO;
->>>>>>> Stashed changes
 import at.ac.tuwien.inso.sepm.ticketline.rest.performance.PerformanceDTO;
 import at.ac.tuwien.inso.sepm.ticketline.rest.ticket.DetailedTicketTransactionDTO;
 import at.ac.tuwien.inso.sepm.ticketline.rest.ticket.TicketDTO;
-import at.ac.tuwien.inso.springfx.SpringFxmlLoader;
-<<<<<<< Updated upstream
 import at.ac.tuwien.inso.springfx.SpringFxmlLoader.LoadWrapper;
-=======
 
-import java.util.ArrayList;
-import java.util.Iterator;
->>>>>>> Stashed changes
 import java.util.List;
 import javafx.collections.ObservableList;
-import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-<<<<<<< Updated upstream
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-=======
-import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> Stashed changes
 
 @Slf4j
 @Component
