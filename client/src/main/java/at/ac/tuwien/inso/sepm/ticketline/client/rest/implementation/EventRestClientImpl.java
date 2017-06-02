5fe4d72d98acf878dd74ceeb8f18f43f0aa76dda
Akkes1 <a.schneider9494@gmail.com>
2017-06-02 14:47:44 +0200

    Service and RestClient implementations

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/rest/implementation/EventRestClientImpl.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/rest/implementation/EventRestClientImpl.java
@@ -2,22 +2,27 @@ package at.ac.tuwien.inso.sepm.ticketline.client.rest.implementation;
 
 import at.ac.tuwien.inso.sepm.ticketline.client.exception.DataAccessException;
 import at.ac.tuwien.inso.sepm.ticketline.client.rest.EventRestClient;
+import at.ac.tuwien.inso.sepm.ticketline.rest.artist.EventArtistDTO;
 import at.ac.tuwien.inso.sepm.ticketline.rest.event.EventDTO;
+import at.ac.tuwien.inso.sepm.ticketline.rest.event.EventSearchDTO;
+import at.ac.tuwien.inso.sepm.ticketline.rest.location.LocationDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @Slf4j
 @Component
 public class EventRestClientImpl implements EventRestClient {
     private static final String EVENT_URL = "/event";
+    private static final String ARTIST_URL = "/artist";
+    private static final String LOCATION_URL = "/location";
 
     private final RestClient restClient;
 
@@ -47,5 +52,91 @@ public class EventRestClientImpl implements EventRestClient {
             throw new DataAccessException(e.getMessage(), e);
         }
     }
+
+    @Override
+    public List<EventDTO> search(EventSearchDTO searchParams, int page) throws DataAccessException {
+        try {
+            // TODO: paging?
+            log.debug("Searching events with event search dto = {}", searchParams);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<EventSearchDTO> entity = new HttpEntity<>(searchParams, headers);
+
+            ResponseEntity<List<EventDTO>> response =
+                restClient.exchange(
+                    UriComponentsBuilder.fromUri(restClient.getServiceURI(EVENT_URL))
+                        .build().toUri(),
+                    HttpMethod.POST,
+                    entity,
+                    new ParameterizedTypeReference<List<EventDTO>>() {
+                    });
+            log.debug("Result status was {} with content {}", response.getStatusCode(),
+                response.getBody());
+            return response.getBody();
+        } catch (HttpStatusCodeException e) {
+            throw new DataAccessException(
+                "Failed to search for customers with status code " + e.getStatusCode().toString());
+        } catch (RestClientException e) {
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public List<EventArtistDTO> searchArtists(String query, int page) throws DataAccessException {
+        try {
+            log.debug("Searching artists with query {} using url {}", query,
+                restClient.getServiceURI(ARTIST_URL) + "?search=" + query);
+            ResponseEntity<List<EventArtistDTO>> artists =
+                restClient.exchange(
+                    UriComponentsBuilder.fromUri(
+                        URI.create(restClient.getServiceURI(ARTIST_URL) + "/search"))
+                        .queryParam("query", query)
+                        .queryParam("page", page)
+                        .queryParam("size", 20)
+                        .build().toUri(),
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<List<EventArtistDTO>>() {
+                    });
+            log.debug("Result status was {} with content {}", artists.getStatusCode(),
+                artists.getBody());
+            return artists.getBody();
+        } catch (HttpStatusCodeException e) {
+            throw new DataAccessException(
+                "Failed to search for artists with status code " + e.getStatusCode().toString());
+        } catch (RestClientException e) {
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public List<LocationDTO> searchLocations(String query, int page) throws DataAccessException {
+        try {
+            log.debug("Searching locations with query {} using url {}", query,
+                restClient.getServiceURI(LOCATION_URL) + "?search=" + query);
+            ResponseEntity<List<LocationDTO>> customer =
+                restClient.exchange(
+                    UriComponentsBuilder.fromUri(
+                        URI.create(restClient.getServiceURI(LOCATION_URL) + "/search"))
+                        .queryParam("query", query)
+                        .queryParam("page", page)
+                        .queryParam("size", 20)
+                        .build().toUri(),
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<List<LocationDTO>>() {
+                    });
+            log.debug("Result status was {} with content {}", customer.getStatusCode(),
+                customer.getBody());
+            return customer.getBody();
+        } catch (HttpStatusCodeException e) {
+            throw new DataAccessException(
+                "Failed to search for locations with status code " + e.getStatusCode().toString());
+        } catch (RestClientException e) {
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
 
 }
