82b0a2414917e25e596bacb31ee158cad11beaec
Benjamin Schaden <benni.schaden@gmail.com>
2017-06-04 16:02:41 +0200

    implemented LocationSearch

--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/repository/LocationRepository.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/repository/LocationRepository.java
@@ -2,10 +2,11 @@ package at.ac.tuwien.inso.sepm.ticketline.server.repository;
 
 import at.ac.tuwien.inso.sepm.ticketline.server.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface LocationRepository extends JpaRepository<Location, UUID> {
+public interface LocationRepository extends JpaRepository<Location, UUID>, QueryDslPredicateExecutor<Location> {
 }
