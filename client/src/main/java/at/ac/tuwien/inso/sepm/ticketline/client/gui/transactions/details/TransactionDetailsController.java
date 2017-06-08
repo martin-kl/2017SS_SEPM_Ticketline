c5f4c94953cc0d9a5512bcbcc92600d0655170eb
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2017-06-08 21:37:10 +0200

    feature: guest users can buy tickets

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/transactions/details/TransactionDetailsController.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/transactions/details/TransactionDetailsController.java
@@ -43,8 +43,12 @@ public class TransactionDetailsController {
         loadButtonsAccordingToStatus(detailedTicketTransactionDTO.getStatus(), false);
 
         transactionNumber.setText(detailedTicketTransactionDTO.getId().toString());
-        customer.setText(detailedTicketTransactionDTO.getCustomer().getFirstName() + " "
-            + detailedTicketTransactionDTO.getCustomer().getLastName());
+        if (detailedTicketTransactionDTO.getCustomer() != null) {
+            customer.setText(detailedTicketTransactionDTO.getCustomer().getFirstName() + " "
+                + detailedTicketTransactionDTO.getCustomer().getLastName());
+        } else {
+            customer.setText(BundleManager.getBundle().getString("customer.guest"));
+        }
         totalPrice.setText(
             "€" + Helper.getTotalPrice(detailedTicketTransactionDTO.getTickets()).toString());
     }
