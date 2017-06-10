91d9c68b434f1eec621f0779c7411546c6643992
Calvin Claus <calvinclaus@me.com>
2017-06-11 00:23:56 +0200

    java doc

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/rest/interceptor/PrincipalRestClient.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/rest/interceptor/PrincipalRestClient.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.inso.sepm.ticketline.client.rest.interceptor;
 
 import at.ac.tuwien.inso.sepm.ticketline.client.exception.DataAccessException;
+import at.ac.tuwien.inso.sepm.ticketline.client.exception.ExceptionWithDialog;
 import at.ac.tuwien.inso.sepm.ticketline.client.exception.ValidationException;
 import at.ac.tuwien.inso.sepm.ticketline.rest.principal.PrincipalDTO;
 
@@ -9,11 +10,39 @@ import java.util.UUID;
 
 public interface PrincipalRestClient {
 
+    /**
+     * finds all principals
+     * @param page
+     * @return
+     * @throws ExceptionWithDialog
+     */
     List<PrincipalDTO> findAll(int page) throws DataAccessException;
 
-    List<PrincipalDTO> search(String query, Boolean locked, int page) throws DataAccessException;
+    /**
+     * searches for principal dto
+     * @param query will fuzzy search in all text fields of principal
+     * @param locked true/false or null if both are to be searched
+     * @param page
+     * @return list of found principals
+     * @throws ExceptionWithDialog
+     */
+    List<PrincipalDTO> search(String query, Boolean locked, int page) throws ExceptionWithDialog;
 
-    PrincipalDTO save(PrincipalDTO principalDTO) throws ValidationException, DataAccessException;
+    /**
+     * saves a principalDTO
+     * @param principalDTO
+     * @return the saved principal
+     * @throws ExceptionWithDialog
+     */
+    PrincipalDTO save(PrincipalDTO principalDTO) throws ExceptionWithDialog;
 
-    PrincipalDTO setLocked(UUID principalId, Boolean locked) throws DataAccessException, ValidationException;
+    /**
+     * sets principal with principalId to value of locked
+     * @param principalId
+     * @param locked
+     * @return the principal
+     * @throws ExceptionWithDialog
+     */
+    PrincipalDTO setLocked(UUID principalId, Boolean locked) throws ExceptionWithDialog;
 }
+
--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/service/implementation/PrincipalServiceImpl.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/service/implementation/PrincipalServiceImpl.java
@@ -21,16 +21,36 @@ public class PrincipalServiceImpl implements PrincipalService {
         this.principalRestClient = principalRestClient;
     }
 
+    /**
+     * finds all principals
+     * @param page
+     * @return
+     * @throws ExceptionWithDialog
+     */
     @Override
     public List<PrincipalDTO> findAll(int page) throws ExceptionWithDialog {
         return principalRestClient.findAll(page);
     }
 
+    /**
+     * searches for principal dto
+     * @param query will fuzzy search in all text fields of principal
+     * @param locked true/false or null if both are to be searched
+     * @param page
+     * @return list of found principals
+     * @throws ExceptionWithDialog
+     */
     @Override
     public List<PrincipalDTO> search(String query, Boolean locked, int page) throws ExceptionWithDialog {
         return principalRestClient.search(query, locked, page);
     }
 
+    /**
+     * saves a principalDTO
+     * @param principalDTO
+     * @return the saved principal
+     * @throws ExceptionWithDialog
+     */
     @Override public PrincipalDTO save(PrincipalDTO principalDTO) throws ExceptionWithDialog {
         if (principalDTO.getUsername().length() < 3) throw new ValidationException("principal.validation.username");
         EmailValidator ev = new EmailValidator();
@@ -43,6 +63,13 @@ public class PrincipalServiceImpl implements PrincipalService {
         return principalRestClient.save(principalDTO);
     }
 
+    /**
+     * sets principal with principalId to value of locked
+     * @param principalId
+     * @param locked
+     * @return the principal
+     * @throws ExceptionWithDialog
+     */
     @Override
     public PrincipalDTO setLocked(UUID principalId, Boolean locked) throws ExceptionWithDialog {
         return principalRestClient.setLocked(principalId, locked);
