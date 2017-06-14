aa96ca4b12a61861b7e8d7681b58ad2c51f4a0e1
Plaudertasche <e1526110@student.tuwien.ac.at>
2017-06-14 20:27:21 +0200

    fixed unwanted change in test data generator which caused some failing tests

--- server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/EventFilterTestDataGenerator.java
+++ server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/EventFilterTestDataGenerator.java
@@ -232,7 +232,7 @@ public class EventFilterTestDataGenerator {
                 30, 0);
 
         cal.setTime(new java.util.Date(System.currentTimeMillis()));
-        cal.add(Calendar.DAY_OF_YEAR, 201); //add 20 days to today
+        cal.add(Calendar.DAY_OF_YEAR, 20); //add 20 days to today
         LocalDateTime performance3Start = LocalDateTime
             .of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), 18,
                 15, 0);
