97d6d142cb93540fff1f19ac80c621007f0b93a4
Martin Klampfer <e1526110@student.tuwien.ac.at>
2017-06-20 09:09:28 +0200

    fixed failing test

--- server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/EventFilterTestDataGenerator.java
+++ server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/EventFilterTestDataGenerator.java
@@ -458,7 +458,7 @@ public class EventFilterTestDataGenerator {
         Calendar cal = Calendar.getInstance();
 
         cal.setTime(new java.util.Date(System.currentTimeMillis()));
-        cal.add(Calendar.DAY_OF_YEAR, -20); //remove 20 days from today
+        cal.add(Calendar.DAY_OF_YEAR, -18); //remove 18 days from today
         LocalDateTime performance1 = LocalDateTime
             .of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), 18,
                 15, 0);
